c1cb68247cbec544f413c6f48993ff911289e8a4
LennartHemmerling <hemmerling@fromscript.de>
2025-01-27 09:50:21 +0100

    added quality of life android features - screen orientation - keep awake - router back

--- packages/mindfull/mindfull-app/src/main.ts
+++ packages/mindfull/mindfull-app/src/main.ts
@@ -5,6 +5,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { KeepAwake } from '@capacitor-community/keep-awake'
+import { ScreenOrientation } from '@capacitor/screen-orientation'
+
+KeepAwake.keepAwake()
+ScreenOrientation.lock({ orientation: 'portrait' })
+
 const app = createApp(App)
 
 app.use(router)
--- packages/mindfull/mindfull-app/src/router/index.ts
+++ packages/mindfull/mindfull-app/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import { App as CapacitorApp } from '@capacitor/app'
+
 import StartScreenVue from '@/views/StartScreenVue.vue'
 import HomeView from '@/views/HomeView.vue'
 import NewItemView from '@/views/NewItemView.vue'
@@ -29,6 +31,15 @@ const router = createRouter({
             component: EditItemView
         }
     ]
+})
+
+// Handle back button
+CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+    if (!canGoBack) {
+        CapacitorApp.exitApp()
+    } else {
+        window.history.back()
+    }
 })
 
 export default router
